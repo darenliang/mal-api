c75a87bc9e5e450d3d7f27f3dc89b8fb98da2d1b
Daren Liang <darenliang@outlook.com>
2019-10-26 13:14:22 -0400

    Add timeout config setting

--- mal/config.py
+++ mal/config.py
@@ -1,3 +1,5 @@
 MAL_ENDPOINT = "https://myanimelist.net/"
 NOT_FOUND_TITLE = "404 Not Found - MyAnimeList.net "
 NO_BACKGROUND_INFO = "No background information has been added to this title. Help improve our database by adding background information here."
+
+TIMEOUT = 5
