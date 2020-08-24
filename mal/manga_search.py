f7a64fb72c7932cbde6f0617fc3eee9053f328f0
Authum09 <billywurow@googlemail.com>
2020-08-24 22:29:41 +0200

    - Added url attribute to MangaSearchResults - added url attributes to README - set version to 0.2.3

--- mal/manga_search.py
+++ mal/manga_search.py
@@ -3,8 +3,8 @@ from mal.search import _Search, _SearchResult
 
 
 class MangaSearchResult(_SearchResult):
-    def __init__(self, image_url, title, synopsis, media_type, volumes, score):
-        super().__init__(image_url, title, synopsis, media_type, score)
+    def __init__(self, url, image_url, title, synopsis, media_type, volumes, score):
+        super().__init__(url, image_url, title, synopsis, media_type, score)
         self.volumes = volumes
 
 
@@ -25,6 +25,7 @@ class MangaSearch(_Search):
             for tr in trs[1:]:
                 tds = tr.find_all("td")
                 results.append(MangaSearchResult(
+                    url=tds[0].find("a")["href"],
                     image_url=tds[0].find("img")["data-src"],
                     title=tds[1].find("strong").text.strip(),
                     synopsis=self._remove_suffix(tds[1].find("div", {"class": "pt4"}).text.strip(), "read more."),
