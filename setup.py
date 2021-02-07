0aa0186bc5291498a65d2de7fe55b7ceb1bb2b19
Daren Liang <daren@darenliang.com>
2021-02-06 21:07:40 -0500

    Update to 0.3.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mal-api",
-    version="0.3.7",
+    version="0.3.8",
     description="A local MyAnimeList API ",
     license="MIT",
     long_description=long_description,
