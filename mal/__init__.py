1c80d2c6727734e134917a25ccf31749be554ac6
zenn <66673988+riyuzenn@users.noreply.github.com>
2022-04-25 23:30:33 +0200

    Include MangaType & AnimeType

--- mal/__init__.py
+++ mal/__init__.py
@@ -1,8 +1,23 @@
-from mal._anime import Anime
+from mal._anime import Anime, AnimeType
 from mal._anime_search import AnimeSearch
-from mal._manga import Manga
+from mal._manga import Manga, MangaType
 from mal._manga_search import MangaSearch
 from mal._anime import AnimeCharacterResult, AnimeStaffResult
 from mal._anime_search import AnimeSearchResult
 from mal._manga import MangaCharacterResult
 from mal._manga_search import MangaSearchResult
+
+
+__all__ = [
+    'Anime',
+    'AnimeType',
+    'AnimeSearch',
+    'Manga',
+    'MangaType',
+    'MangaSearch',
+    'AnimeCharacterResult', 
+    'AnimeStaffResult',
+    'AnimeSearchResult',
+    'MangaCharacterResult',
+    "MangaSearchResult"
+]
