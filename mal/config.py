7ae37413d95d8b15da80125296fb21877991ba23
zenn <66673988+riyuzenn@users.noreply.github.com>
2022-04-25 23:29:15 +0200

    Update config.py

--- mal/config.py
+++ mal/config.py
@@ -1,2 +1,21 @@
 MAL_ENDPOINT = "https://myanimelist.net/"
 TIMEOUT = 5
+MAL_ADVANCED_SEARCH = {
+    "anime": {
+        "tv":       1,
+        "ova":      2,
+        "movie":    3,
+        "special":  4,
+        "ona":      5,
+        "music":    6
+    },
+    "manga": {
+        "manga":        1,
+        "one-shot":     2,
+        "doujinshi":    3,
+        "light novel":  4,
+        "novel":        5,
+        "manhwa":       6,
+        "manhua":       7
+    }
+}
